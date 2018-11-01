ff619eab894fcaf6275d1378f41028d7ab3b1ff0
Renan Ugalde <renan@muuklabs.com>
2018-10-31 18:55:18 -0600

    Adding changes to support new route download_byproperty

--- mkcli.py
+++ mkcli.py
@@ -5,70 +5,69 @@ import subprocess
 from urllib import request
 import requests
 import json
+import urllib
 
 def run(args):
   #Gets the value from the flags
   field = args.field
   value = args.value
+  noexec = args.noexec
   route = 'src/test/groovy'
   dirname = os.path.dirname(__file__)
+  if field == "hashtag":
+    value = "#"+value
+
+  # print (value)
+  # print (noexec)
+  valueArr = []
+  valueArr.append(value)
+  valueArr.append("")
+  # print(valueArr)
 
   # print(field,value)
 
-  allowed_fields = ['tag','name']
+  allowed_fields = ['tag','name', 'hashtag']
   if field in allowed_fields:
-    # #Delete the old files 
+    # #Delete the old files
     if os.path.exists("test.rar"):
       os.remove('test.rar')
     shutil.rmtree(route, ignore_errors=True)
     if not os.path.exists(route):
       os.makedirs(route)
 
-    # We get the id
-    r = requests.post(url = 'http://localhost:8081/get_test_id_from_property/', data = {'property': field, 'value': value})
-    resp = r.json()
-    
-    id = ''
-    success = False
-    message = ''
-    for key, value in resp.items():
-      if key == 'id':
-        id = value
-      if key == 'success':
-        success = value
-      if key == 'message':
-        message = value
-      
-    if success:
-      # Download the file
-      url = "http://localhost:8081/download_script_bytest/"+id  
-      response = request.urlopen(url)
-      file = response.read()
-      fileobj = open('test.rar',"wb")
-      fileobj.write(file)
-      fileobj.close()
+    values = {'property': field, 'value': valueArr}
+    #print (values)
+    # This route downloads the scripts by the property.
+    url = 'http://localhost:8081/download_byproperty/'
+    data = urllib.parse.urlencode(values, doseq=True).encode('UTF-8')
 
-      # Unzip the file // the library needs the file to end in .rar for some reason 
-      shutil.unpack_archive('test.rar', extract_dir=route, format='zip')
+    # now using urlopen get the file and store it locally
+    response = request.urlopen(url,data)
+    file = response.read()
+    fileobj = open('test.zip',"wb")
+    fileobj.write(file)
+    fileobj.close()
 
-      #Executes the test
+    # Unzip the file // the library needs the file to end in .rar for some reason
+    shutil.unpack_archive('test.zip', extract_dir=route, format='zip')
+
+    if noexec == False :
+      #Execute the test
       os.system(dirname + '/gradlew clean test')
-    else:
-      print(message)
   else:
     print(field+': is not an allowed property')
 
- 
+
 
 
 def main():
-	parser=argparse.ArgumentParser(description="Convert a fastA file to a FastQ file")
-	parser.add_argument("-p",help="property to search the test for" ,dest="field", type=str, required=True)
-	parser.add_argument("-t",help="value of the test field" ,dest="value", type=str, required=True)
-	parser.set_defaults(func=run)
-	args=parser.parse_args()
-	args.func(args)
+    parser=argparse.ArgumentParser(description="MuukTest cli to download tests from the cloud")
+    parser.add_argument("-p",help="property to search the test for" ,dest="field", type=str, required=True)
+    parser.add_argument("-t",help="value of the test or hashtag field" ,dest="value", type=str, required=True)
+    parser.add_argument("-noexec",help="(Optional). If set then only download the scripts", dest="noexec", action="store_true")
+    parser.set_defaults(func=run)
+    args=parser.parse_args()
+    args.func(args)
 
 if __name__=="__main__":
 	main()
-
