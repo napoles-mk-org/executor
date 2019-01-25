4a84e1179d3171c58fd3833e7aa920d7e2e11cf9
Armando Martinez Mandujano <armando@muuklabs.com>
2019-01-24 19:17:34 -0600

    Adding the response message instead show error

--- mkcli.py
+++ mkcli.py
@@ -39,7 +39,7 @@ def run(args):
   except:
     print("Key file was not found on the repository (Download it from the Muuktest portal)")
     exit()
- 
+
   auth = {'Authorization': 'Bearer ' + token}
 
   allowed_fields = ['tag','name', 'hashtag']
@@ -63,17 +63,28 @@ def run(args):
 
     # response = request.urlopen(url,data)
     file = response.read()
-    fileobj = open('test.zip',"wb")
-    fileobj.write(file)
-    fileobj.close()
+    flag = False;
 
-    # Unzip the file // the library needs the file to end in .rar for some reason
-    shutil.unpack_archive('test.zip', extract_dir=route, format='zip')
+    try:
+        decode_text = file.decode("utf-8")
+        json_decode = json.loads(file.decode("utf-8"))
+        print(json_decode["message"])
+    except:
+        flag = True
 
-    os.system('chmod 544 ' + dirname + '/gradlew')
-    if noexec == False :
-      #Execute the test
-      os.system(dirname + '/gradlew clean test')
+    if (flag == True):
+        fileobj = open('test.zip',"wb")
+        fileobj.write(file)
+        fileobj.close()
+
+        # Unzip the file // the library needs the file to end in .rar for some reason
+        shutil.unpack_archive('test.zip', extract_dir=route, format='zip')
+
+        os.system('chmod 544 ' + dirname + '/gradlew')
+        if noexec == False :
+          #Execute the test
+          os.system(dirname + '/gradlew clean test')
+
   else:
     print(field+': is not an allowed property')
 
