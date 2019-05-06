00dc618b275b8145d40e1b90d28f62d944e07f81
Renan Ugalde <renan@Renans-MacBook-Pro.local>
2019-05-05 22:02:42 -0500

    Changes for Beta 2.0. This includes the new routes, tracking improvements and a minor fix that impacted the feedback for multitests

--- mkcli.py
+++ mkcli.py
@@ -7,13 +7,15 @@ import requests
 import json
 import urllib
 import xml.etree.ElementTree
+#import ssl
 
 def gatherFeedbackData():
   path = 'build/test-results/chromeTest'
-  testSuccess = True
-  error = ''
+
   feedbackData = []
   for filename in os.listdir(path):
+    testSuccess = True
+    error = ''
     if filename.endswith('.xml'):
       e = xml.etree.ElementTree.parse('build/test-results/chromeTest/' + filename).getroot()
 
@@ -46,8 +48,8 @@ def run(args):
   value = args.value
   noexec = args.noexec
   route = 'src/test/groovy'
-  muuktestRoute = 'http://ec2-3-17-71-29.us-east-2.compute.amazonaws.com:8081/'
-  supportRoute = 'http://ec2-18-219-8-121.us-east-2.compute.amazonaws.com:8082/'
+  muuktestRoute = 'https://portal.muuktest.com:8081/'
+  supportRoute = 'https://testing.muuktest.com:8082/'
 
   # muuktestRoute = 'http://localhost:8081/'
   # supportRoute = 'http://localhost:8082/'
@@ -71,12 +73,14 @@ def run(args):
     key_file = open(path,'r')
     key = key_file.read()
     r = requests.post(muuktestRoute+"generate_token_executer", data={'key': key})
+    #r = requests.post(muuktestRoute+"generate_token_executer", data={'key': key}, verify=False)
     responseObject = json.loads(r.content)
     token = responseObject["token"]
     userId = responseObject["userId"]
     organizationId = responseObject["organizationId"]
-  except:
+  except Exception as ex:
     print("Key file was not found on the repository (Download it from the Muuktest portal)")
+    print (ex)
     exit()
 
   auth = {'Authorization': 'Bearer ' + token}
@@ -94,12 +98,14 @@ def run(args):
     values = {'property': field, 'value[]': valueArr, 'userId': userId}
     # This route downloads the scripts by the property.
     url = muuktestRoute+'download_byproperty/'
+    #context = ssl._create_unverified_context()
     data = urllib.parse.urlencode(values, doseq=True).encode('UTF-8')
 
     # now using urlopen get the file and store it locally
     auth_request = request.Request(url,headers=auth, data=data)
     auth_request.add_header('Authorization', 'Bearer '+token)
     response = request.urlopen(auth_request)
+    #response = request.urlopen(auth_request, context=context)
 
     # response = request.urlopen(url,data)
     file = response.read()
@@ -135,6 +141,7 @@ def run(args):
 
         try:
           requests.post(supportRoute+"tracking_data", json=payload)
+          #requests.post(supportRoute+"tracking_data", json=payload, verify=False)
         except Exception as e:
           print("Not connection to support Data Base");
 
@@ -147,9 +154,9 @@ def run(args):
           url = muuktestRoute+'feedback/'
           values = {'tests': testsExecuted, 'userId': userId}
           hed = {'Authorization': 'Bearer ' + token}
-          requests.post(url, json=values, headers=hed)
-
           try:
+            requests.post(url, json=values, headers=hed)
+            #requests.post(url, json=values, headers=hed, verify=False)
             # save the executed test entry to the database
             requests.post(supportRoute+"tracking_data", data={
               'action': 3,
