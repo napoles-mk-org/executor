901112157719f5eb16269e3e89cf262f0808a910
unknown <valentin@muuklabs.com>
2019-08-13 16:25:05 -0700

    Fix style for GebConfig file

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,6 +1,5 @@
 /*
 	This is the Geb configuration file.
-	
 	See: http://www.gebish.org/manual/current/#configuration
 */
 
@@ -10,37 +9,33 @@ import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
 
 waiting {
-	timeout = 2
+  timeout = 2
 }
 
 environments {
-	
-	// run via “./gradlew chromeTest”
-	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
-	chrome {
-		driver = { new ChromeDriver() }
-	}
+  // run via “./gradlew chromeTest”
+  // See: http://code.google.com/p/selenium/wiki/ChromeDriver
+  chrome {
+    driver = { new ChromeDriver() }
+  }
 
-	// run via “./gradlew chromeHeadlessTest”
-	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
-	chromeHeadless {
-		driver = {
-			ChromeOptions o = new ChromeOptions()
-			o.addArguments('headless')
-			new ChromeDriver(o)
-		}
-	}
+  // run via “./gradlew chromeHeadlessTest”
+  // See: http://code.google.com/p/selenium/wiki/ChromeDriver
+  chromeHeadless {
+    driver = {
+      ChromeOptions o = new ChromeOptions()
+      o.addArguments('headless')
+      new ChromeDriver(o)
+    }
+  }
 	
-	// run via “./gradlew firefoxTest”
-	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
-	firefox {
+  // run via “./gradlew firefoxTest”
+  // See: http://code.google.com/p/selenium/wiki/FirefoxDriver
+  firefox {
 		atCheckWaiting = 1
-
-		driver = { new FirefoxDriver() }
-	}
-
+    driver = { new FirefoxDriver() }
+  }
 }
 
 // To run the tests with all browsers just run “./gradlew test”
-
 baseUrl = "http://gebish.org"
