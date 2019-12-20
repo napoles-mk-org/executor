a34e0943fc86482c8bf83d18c9ba61c18541b026
Nilson Lagos <nilson@muuklabs.com>
2019-12-20 16:27:03 -0600

    test

--- mkcli.py
+++ mkcli.py
@@ -155,10 +155,10 @@ def run(args):
         if noexec == False :
           #Execute the test
           print("Executing test...")
-          # os.system("tmux new-session -d -s BehatRecording1 'ffmpeg -f x11grab -video_size 1280x1024 -i :99 -codec:v libx264 -r 12 video.mp4'")
+          os.system("tmux new-session -d -s BehatRecording1 'ffmpeg -f x11grab -video_size 1280x1024 -i :99 -codec:v libx264 -r 12 video.mp4'")
           os.system(dirname + '/gradlew clean '+browserName)
-          # os.system("tmux send-keys -t BehatRecording1 q")
-          # os.system("tmux kill-session -t BehatRecording1")
+          os.system("tmux send-keys -t BehatRecording1 q")
+          os.system("tmux kill-session -t BehatRecording1")
 
           testsExecuted = gatherFeedbackData(browserName)
           url = muuktestRoute+'feedback/'
