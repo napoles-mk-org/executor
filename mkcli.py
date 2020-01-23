00eb29e0fc75fa109d86b942d363c9ae96cb121b
nilsonlagos <nilson@muuklabs.com>
2020-01-23 17:34:43 -0600

    test

--- mkcli.py
+++ mkcli.py
@@ -178,7 +178,7 @@ def run(args):
           #Execute the test
           print("Executing test...")
           # os.system("tmux new-session -d -s Muukrecording 'ffmpeg -f x11grab -video_size 1280x1024 -i :99 -codec:v libx264 -r 25  -color_primaries smpte170m -color_trc smpte170m -colorspace smpte170m  " + str(organizationId) + "_" + str(executionNumber) + ".mp4'")
-          os.system("tmux new-session -d -s Muukrecording 'ffmpeg -f x11grab -video_size 1280x1024 -i :99 -codec:v ffv1 -r 12 " + str(organizationId) + "_" + str(executionNumber) + ".mp4'")
+          os.system("tmux new-session -d -s Muukrecording 'ffmpeg -f x11grab -video_size 1280x1024 -i :99 -codec:v libx264 -r 12 " + str(organizationId) + "_" + str(executionNumber) + ".mp4'")
 
           # os.system("tmux new-session -d -s Muukrecording 'ffmpeg -f x11grab -video_size 1280x1024 -i :99 -codec:v libx264 -r 12 " + str(organizationId) + "_" + str(executionNumber) + ".mp4'")
           exitCode = subprocess.call(dirname + '/gradlew clean '+browserName, shell=True)
