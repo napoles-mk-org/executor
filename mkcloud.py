ab83cf817d5eb0920d80f4a8dbe0a622bc4e5cb6
nilsonlagos <nilson@muuklabs.com>
2020-03-09 14:35:03 -0600

    image reduction to 60%

--- mkcloud.py
+++ mkcloud.py
@@ -8,7 +8,7 @@ def resizeImages(browserName):
       if filename.endswith('.png'):
         im = Image.open(path+"/"+filename)
         size =im.size   # get the size of the input image
-        ratio = 0.4  # reduced the size to 40% of the input image
+        ratio = 0.60  # reduced the size to 60% of the input image
         reduced_size = int(size[0] * ratio), int(size[1] * ratio)
 
         im_resized = im.resize(reduced_size, Image.ANTIALIAS)
