a535372ea52b4bb39dafdef282c64b0369520d5d
Naps <89811779+napoles-mk@users.noreply.github.com>
2022-04-18 11:31:35 -0500

    Video enabled (#26)
    * adding changes to support video and add ssl cert ignore flags
    
    * fixing header
    
    * changes to verify the recording
    
    * fixing gebconfig file
    
    * adding changes for video
    
    * adding logs for debugging
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * default settings
    
    * changes
    
    * headers
    
    * adding threads
    
    * Adding json-simple dependency
    
    Co-authored-by: Renan Ugalde <renan@Renans-MacBook-Pro.local>
    Co-authored-by: nilson-lagos <42681008+nilson-lagos@users.noreply.github.com>

--- mkcli.py
+++ mkcli.py
@@ -9,6 +9,7 @@ import urllib
 import xml.etree.ElementTree
 from time import strftime
 from mkcloud import gatherScreenshots, resizeImages
+from mkvideo import Video
 #import ssl
 
 def getExitCodeFromSuccessCriteria(successCriteria, results):
@@ -216,13 +217,24 @@ def run(args):
 
       if noexec == False :
         #Execute the test
+        videoNameFile = str(organizationId) + "_" + str(executionNumber) + ".mp4"
+        print("File name for video: " + videoNameFile)
         print("Executing test...")
         try:
+          v = Video()
+          v.checkAndStartRecording(videoNameFile)
+          v.checkActiveSession()
+          v.executeCmd("ps -ef | grep ffmpeg")
+          v.executeCmd("ls -ltr | grep *.mp4")
           exitCode = subprocess.call(dirname + '/gradlew clean '+browserName, shell=True)
         except Exception as e:
           print("Error during gradlew compilation and/or execution ")
           print(e)
 
+        v.executeCmd("ps -ef | grep ffmpeg")
+        v.checkAndStopRecording()
+        v.executeCmd("ls -ltr | grep *.mp4")
+        del v
         testsExecuted = gatherFeedbackData(browserName)
         url = muuktestRoute+'feedback/'
         values = {'tests': testsExecuted, 'userId': userId, 'browser': browserName,'executionNumber': int(executionNumber)}
@@ -234,8 +246,13 @@ def run(args):
         filesData = gatherScreenshots(browserName)
         try:
           if filesData != {}:
+            print("Upload the screenshots: ")
             requests.post(muuktestRoute + 'upload_cloud_steps_images/', headers=hed, files = filesData)
             #requests.post(muuktestRoute + 'upload_cloud_steps_images/', data={'cloudKey': cloudKey}, headers=hed, files = filesData, verify=False)
+            print("Upload the Video: ")
+            videoFile = open(videoNameFile, 'rb')
+            files = {'file': videoFile}
+            requests.post(muuktestRoute + 'upload_video/', headers=hed, files=files)
           else:
             print ("filesData empty.. cannot send screenshots")
         except Exception as e:
